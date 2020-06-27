92b46d047037f8b55338e58c25c069f05285fa6e
Licolnlee <me@licolnlee.icu>
2020-06-27 21:23:32 +0800

    log4j plugin update

--- src/main/java/com/itheima/dao/IUserDao.java
+++ src/main/java/com/itheima/dao/IUserDao.java
@@ -5,6 +5,26 @@ import com.itheima.domain.User;
 import java.util.List;
 
 public interface IUserDao {
+    /*
+    * 查询所有用户
+    * */
 
     List<User> findAll();
+
+    /*保存用户*/
+    void saveUser(User user);
+
+    /*更新用户*/
+    void updateUser(User user);
+
+    /*根据ID删除用户*/
+    void deleteUser(Integer userId);
+
+    User findById(Integer userId);
+
+    /*根据名称模糊查询用户信息*/
+    List<User> findByName(String username);
+
+    /*查询总用户数*/
+    int findTotal();
 }
--- src/test/java/com/itheima/test/MybatisTest.java
+++ src/test/java/com/itheima/test/MybatisTest.java
@@ -6,29 +6,106 @@ import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
 
 import java.io.InputStream;
+import java.util.Date;
 import java.util.List;
 
 public class MybatisTest {
-    /*test*/
-    public static void main(String[] args) throws Exception {
-        /*reading config */
+
+    private InputStream ins;
+    private SqlSession sqlSession;
+    private IUserDao userDao;
+
+    @Before//用于在测试方法执行之前执行
+    public void init() throws Exception{
         /*create sqlsessionfactory*/
-        InputStream ins = Resources.getResourceAsStream("SqlMapConfig.xml");
+        ins = Resources.getResourceAsStream("SqlMapConfig.xml");
         /*use factory generate sqlsession object*/
-        SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
-        SqlSessionFactory factory = builder.build(ins);
+        SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(ins);
         /*user sqlsession to create dao interface proxy object*/
-        SqlSession session = factory.openSession();
-        IUserDao userDao = session.getMapper(IUserDao.class);
+        sqlSession = factory.openSession();
+        userDao = sqlSession.getMapper(IUserDao.class);
+    }
+    @After//用于在测试方法执行之后执行
+    public void destory() throws Exception{
+        /*释放资源*/
+        sqlSession.close();
+        ins.close();
+    }
+    /*test*/
+    @Test
+    public void testFindAll(){
+        /*reading config */
         List<User> users = userDao.findAll();
         /*user proxy object perform function*/
         for (User user : users){
             System.out.println(user);
         }
-        /*release resources*/
-        session.close();
-        ins.close();
+    }
+
+    /*测试保存操作*/
+
+    @Test
+    public void testSave(){
+        User user = new User();
+        user.setUsername("mybatis saveuser");
+        user.setAddress("北京市顺义区");
+        user.setSex("男");
+        user.setBirthday(new Date());
+
+
+        userDao.saveUser(user);
+    }
+
+    @Test
+    public void testUpdate(){
+        User user = new User();
+        user.setId(50);
+        user.setUsername("mybatis saveuser");
+        user.setAddress("北京市顺义区");
+        user.setSex("女");
+        user.setBirthday(new Date());
+
+
+        userDao.updateUser(user);
+    }
+
+    /*删除用户数据*/
+    @Test
+    public void testDelete(){
+
+        userDao.deleteUser(50);
+    }
+
+    /*查询用户数据*/
+    @Test
+    public void testFindOne(){
+
+        User user = userDao.findById(48);
+        System.out.println(user);
+    }
+
+    /*测试模糊查询操作*/
+    @Test
+    public void testFindName(){
+
+        List<User> users = userDao.findByName("%王%");
+        for (User user:users){
+            System.out.println(user);
+        }
+    }
+
+    /*测试查询总记录条数*/
+    @Test
+    public void testFindTotal(){
+
+        int count = userDao.findTotal();
+        System.out.println(count);
     }
 }
+
+
